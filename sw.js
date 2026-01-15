5ef88ca74b2c64fcd3b25b5001fae08142b61f3c
kendoll-keisuke0408 <keisuke0408ksk@gmail.com>
2026-01-15 22:37:05 +0900

    Deploy v5.0 Hard Reset

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'family-app-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon.svg'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    // Simple Cache First Strategy
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
